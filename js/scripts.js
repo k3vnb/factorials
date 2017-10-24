70527b901772b2ef826873fe1b43e242b208d324
Kevin Boyle and Lena Ha <kevin@>
2017-10-24 16:40:42 -0700

    add alert for NaN entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("form#addnumber").submit(function(event) {
     var i = parseInt($("input#numero").val());
+
+    if (isNaN(i)) {
+      alert("please enter a number");
+    };
     function factorial(n) {
       if (n === 0) {
         return 1;
